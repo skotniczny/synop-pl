c1274e4a5688f84b65f516bd12fcfa72f9af6f46
Łukasz <skotniczny@outlook.com>
2026-07-23 23:08:11 +0200

    feat: filter stations to match synop data and update station type

--- scripts/fetch-stations.js
+++ scripts/fetch-stations.js
@@ -1,23 +1,30 @@
-import { writeFile } from 'node:fs/promises'
+import { writeFile } from "node:fs/promises"
 
 try {
-  const request = await fetch('https://klimat.imgw.pl/json/stacje.json')
-  if (!request.ok) throw Error('Request failed')
-  const data = await request.json()
-  const stations = data
-    .filter(station => station.show === 1 && (station["data_do"] === "" || station["data_do"] === station["data_do"]))
-    .map(station => {
+  const [stationRequest, synopRequest] = await Promise.all([
+    fetch("https://klimat.imgw.pl/json/stacje.json"),
+    fetch("https://danepubliczne.imgw.pl/api/data/synop/"),
+  ])
+
+  if (!stationRequest.ok || !synopRequest.ok) throw Error("Request failed")
+  const [stationData, synopData] = await Promise.all([stationRequest.json(), synopRequest.json()])
+  const synopMap = new Map(synopData.map((item) => [Number(item.id_stacji?.slice(2)), item]))
+  const stations = stationData
+    .filter((station) => synopMap.has(station["kod stacji"]) &&
+      station.show === 1 && (station["data_do"] === "" || station["data_do"] === station["data_od"]))
+    .map((station) => {
+      const synopStation = synopMap.get(station["kod stacji"])
       return {
         id: `12${station["kod stacji"]}`,
+        station: synopStation.stacja,
         name: station["nazwa stacji"],
-        longitude: parseFloat(station["longitude"].replace(",", ".")),
-        latitude:  parseFloat(station["latitude"].replace(",", ".")),
-        altitude: station["wysokość [m npm]"]
+        longitude: Number(station["longitude"].replace(",", ".")),
+        latitude: Number(station["latitude"].replace(",", ".")),
+        altitude: station["wysokość [m npm]"],
       }
-  })
-  await writeFile('./src/data/imgw-stations.json', JSON.stringify(stations, null, 2))
+    })
+  await writeFile("./src/data/imgw-stations.json", JSON.stringify(stations, null, 2))
   console.log("Stations data saved!")
 } catch (e) {
   console.error(e)
 }
-
--- src/data/imgw-stations.ts
+++ src/data/imgw-stations.ts
@@ -1,11 +1,14 @@
-import STATIONS from "./imgw-stations.json" with { type: "json" }
+import STATIONS_JSON from "./imgw-stations.json" with { type: "json" }
 
 type Station = {
   id: string
+  station: string
   name: string
   longitude: number
   latitude: number
   altitude: number
 }
 
-export const stationMap = new Map<string, Station>(STATIONS.map((station) => [station.id, station]))
+export const stations = STATIONS_JSON as Station[]
+
+export const stationMap = new Map<string, Station>(stations.map((station) => [station.id, station]))
