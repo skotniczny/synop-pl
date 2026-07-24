4da97212270f52b8819b6ff33bd163dafb3cde3b
Łukasz <skotniczny@outlook.com>
2026-07-24 13:10:37 +0200

    feat: handle errors when fetching data and display toast notification

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,16 @@ import { initControls } from "./ui/initControls.ts"
 import { initHighlightWidget } from "./ui/highlightwidget/highlightWidget.ts"
 import { layerKeys } from "./map/layerSwitcher.ts"
 import { state } from "./state/appState.ts"
+import { toastDanger } from "./ui/toast/toast.ts"
 
-const data = await fetchImgwSynopData()
-
+const data = await fetchImgwSynopData().catch((e) => {
+  const message =
+    e instanceof Error
+      ? `Nie udało się pobrać danych: ${e.message}`
+      : "Wystąpił nieoczekiwany błąd podczas pobierania danych"
+  toastDanger(message)
+  return []
+})
 const map = initMap("map", toGeoJSON(data))
 initDataTable("#imgwData", data)
 initDateTime(document.body, data)
