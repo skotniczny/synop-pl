00f0878d6f0a71380b2726667b6e298ed784e15d
Łukasz <skotniczny@outlook.com>
2026-08-01 19:33:01 +0200

    refactor: split dataTable into init/update

--- src/ui/datatable/dataTable.ts
+++ src/ui/datatable/dataTable.ts
@@ -4,30 +4,49 @@ import type { DataRecord } from "../../map/config"
 import "datatables.net-dt/css/dataTables.dataTables.min.css"
 import "./dataTable.css"
 
+let table: InstanceType<typeof DataTable> | null = null
+let tableSelector: string
+
+const tableConfig = {
+  paging: true,
+  pageLength: 100,
+  language: {
+    info: "Wyświetlanie wierszy od _START_ do _END_ z _TOTAL_",
+    infoEmpty: "Brak dostępnych wierszy",
+    emptyTable: "Brak danych",
+    infoFiltered: "(filtrowano z _MAX_ wszystkich wierszy)",
+    lengthMenu: "Pokaż _MENU_ wierszy na stronę",
+    search: "Przeszukaj: ",
+    zeroRecords: "Nic nie znaleziono",
+  },
+  scrollX: true,
+  scrollCollapse: true,
+  scrollY: "calc(100vh - 158px)",
+}
+
 export function initDataTable(selector: string, data: DataRecord[]) {
-  new DataTable(selector, {
-    data: data,
+  tableSelector = selector
+  table = new DataTable(selector, {
+    data,
     columns: config.tableColumns,
-    paging: false,
-    language: {
-      info: "Wyświetlanie wierszy od _START_ do _END_ z _TOTAL_",
-      infoEmpty: "Brak dostępnych wierszy",
-      emptyTable: "Brak danych",
-      infoFiltered: "(filtrowano z _MAX_ wszystkich wierszy)",
-      lengthMenu: "Pokaż _MENU_ wierszy na stronę",
-      search: "Przeszukaj: ",
-      zeroRecords: "Nic nie znaleziono",
-    },
-    scrollX: true,
-    scrollCollapse: true,
-    scrollY: "calc(100vh - 158px)",
+    ...tableConfig,
   })
 
-  const dataView: HTMLDivElement | null = document.querySelector<HTMLDivElement>(".data")
+  const dataView = document.querySelector<HTMLDivElement>(".data")
 
   if (state.datatableVisible) {
     dataView?.classList.add("show")
   } else {
     dataView?.classList.remove("show")
   }
+}
+
+export function updateDataTable(data: DataRecord[]) {
+  if (table) table.destroy()
+  document.querySelector(tableSelector)?.replaceChildren()
+  table = new DataTable(tableSelector, {
+    data,
+    columns: config.tableColumns,
+    ...tableConfig,
+  })
 }
