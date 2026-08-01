bdb741e80f7fd9bb1739368ff29ecfa873f0f920
Łukasz <skotniczny@outlook.com>
2026-08-01 19:13:04 +0200

    refactor: split initDateTime into init/update

--- src/ui/datetime/initDateTime.ts
+++ src/ui/datetime/initDateTime.ts
@@ -3,11 +3,28 @@ import { dateTimeComponent } from "./dateTime"
 import { mostFrequent } from "../../utils/mostFrequent"
 import type { DataRecord } from "../../map/config"
 
-export function initDateTime(el: HTMLElement, data: DataRecord[]) {
+let timeEl: HTMLTimeElement
+
+function makeMeasureDateISOString(data: DataRecord[]): string {
+  let dateISOString = new Date().toISOString()
   const measureDateISOString = data
     .filter((item) => item.data_pomiaru && item.godzina_pomiaru)
     .map((item) => makeDateISOString(item.data_pomiaru!, item.godzina_pomiaru!))
-  if (!measureDateISOString.length) return
-  const dateISOString = mostFrequent<string>(measureDateISOString)
-  el.append(dateTimeComponent(dateISOString))
+  if (measureDateISOString.length) {
+    dateISOString = mostFrequent<string>(measureDateISOString)
+  }
+  return dateISOString
+}
+
+export function initDateTime(el: HTMLElement, data: DataRecord[]) {
+  const dateISOString = makeMeasureDateISOString(data)
+  timeEl = dateTimeComponent(dateISOString)
+  el.append(timeEl)
+}
+
+export function updateDateTime(data: DataRecord[]) {
+  const dateISOString = makeMeasureDateISOString(data)
+  const newEl = dateTimeComponent(dateISOString)
+  timeEl.replaceWith(newEl)
+  timeEl = newEl
 }
