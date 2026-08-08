06758eda39e08bab79676f826f5fd0a6e6339aa7
Łukasz <skotniczny@outlook.com>
2026-08-08 16:43:15 +0200

    refactor: extract dateTime styles to component css

--- src/ui/datetime/dateTime.ts
+++ src/ui/datetime/dateTime.ts
@@ -1,10 +1,11 @@
 import { dateTimeFormatWeekday, dateTimeFormatTime, dateTimeFormatDate } from "../../utils/formats"
 import { elt } from "../dom"
+import "./dateTime.css"
 
 export function dateTimeComponent(dateISOString: string): HTMLTimeElement {
   const date = new Date(dateISOString)
-  const weekdayEl = elt("span", { className: "text-capitalize d-block display-1" }, dateTimeFormatWeekday.format(date))
-  const dateEl = elt("span", { className: "datetime" }, dateTimeFormatDate.format(date))
-  const timeEl = elt("span", { className: "time d-block display-2" }, dateTimeFormatTime.format(date))
-  return elt("time", { dateTime: dateISOString, className: "text-right" }, weekdayEl, dateEl, timeEl)
+  const weekdayEl = elt("span", { className: "datetime_weekday display-1" }, dateTimeFormatWeekday.format(date))
+  const dateEl = elt("span", { className: "datetime_date" }, dateTimeFormatDate.format(date))
+  const timeEl = elt("span", { className: "datetime_time display-2" }, dateTimeFormatTime.format(date))
+  return elt("time", { dateTime: dateISOString, className: "datetime text-right" }, weekdayEl, dateEl, timeEl)
 }
